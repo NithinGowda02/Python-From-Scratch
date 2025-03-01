af34c742f60f6f8f2a6957380486421b89ff26f0
Nithin K P <nithinkp0212@gmail.com>
2025-03-01 22:25:03 +0530

    "Implemented basic arithmetic operations in Python"

---
+++ 2-Arithmetic_operations.py
@@ -0,0 +1,9 @@
+first_number = 20
+second_number = 15
+print("ARITHMETIC OPERATIONS")
+print("---------------------")
+print(f"First-Number >> {first_number}\nSecond-Number >> {second_number}")
+print(f"Addition of two number is >> {first_number + second_number}")
+print(f"Substraction  of two number is >> {first_number - second_number}")
+print(f"Multiplication of two number is >> {first_number * second_number}")
+print(f"Division  of two number is >> {first_number / second_number}")
