765ad0c7a20ac41eab4725ec52f3a9b59d5df0bd
Nithin K P <nithinkp0212@gmail.com>
2025-03-01 22:21:21 +0530

    "Learning Python: Variables and data types basics"

---
+++ Swap-Two_variables.py
@@ -0,0 +1,5 @@
+Name1 = "M S DHONI"
+Name2 = "VIRAT KOHLI"
+print(f"Before swapping two variables \nName1 >> {Name1}\nName2 >> {Name2}")
+Name1,Name2=Name2,Name1
+print(f"After swapping two variables \nName1 >> {Name1}\nName2 >> {Name2}")
