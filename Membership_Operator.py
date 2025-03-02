a2db9eeb3ad048f49b4a7923f98bd7c305e14b41
Nithin K P <nithinkp0212@gmail.com>
2025-03-02 22:34:38 +0530

    Added Membership Operator exercises for practice

---
+++ Membership_Operator.py
@@ -0,0 +1,14 @@
+"""
+**Membership Operator Exercise**:
+   Write a Python program that:
+   - Takes a string as input from the user.
+   - Checks if the letter 'a' is in the string (using `in`).
+   - Checks if the string doesn't contain the word "Python" (using `not in`).
+
+"""
+
+User_input = input("Enter a String >> ")
+print(f"If the letter 'a' is in the string >>{'a' in User_input}")
+print(f"if the string doesn't contain the word 'Python' >>{'Python' not in User_input}")
+
+    
