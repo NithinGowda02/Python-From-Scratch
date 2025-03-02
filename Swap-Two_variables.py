3b5bc3294413f05dac32ec71396fccc9ac86581f
Nithin K P <nithinkp0212@gmail.com>
2025-03-02 19:41:54 +0530

    Update Swap-Two_variables.py

--- Swap-Two_variables.py
+++ Swap-Two_variables.py
@@ -1,3 +1,8 @@
+"""
+**Swap Two Variables:**
+   Write a Python program that swaps the values of two variables with and without using a third variable.
+"""
+
 Name1 = "M S DHONI"
 Name2 = "VIRAT KOHLI"
 print(f"Before swapping two variables \nName1 >> {Name1}\nName2 >> {Name2}")
