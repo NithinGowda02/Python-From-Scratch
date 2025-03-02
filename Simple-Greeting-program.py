275cc93e0060b271c8935182ac05891151e1f950
Nithin K P <nithinkp0212@gmail.com>
2025-03-02 19:34:15 +0530

    Added a simple Python program for greeting the user

---
+++ Simple-Greeting-program.py
@@ -0,0 +1,14 @@
+""" 
+**Simple Greeting Program**:
+   Write a Python program that asks the user for their name and age, then prints a personalized greeting message. Use both the `+` operator and f-strings for output.
+**Example**:
+   ```python
+   Enter your name: Alice
+   Enter your age: 25
+   Output: Hello, Alice! You are 25 years old.
+   ```
+"""
+
+name = input("Enter your Name >> ")
+age = input("Enter your Age >> ")
+print(f"Hello, {name}! You are {age} years old.")
