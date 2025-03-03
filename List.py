7a1916754be70e0af35690d672660599fcb47101
Nithin K P <nithinkp0212@gmail.com>
2025-03-03 21:47:48 +0530

    Updated List Exercise

---
+++ List.py
@@ -0,0 +1,12 @@
+"""
+**List Manipulation Exercise**:
+   - Create a list of 5 items (strings or numbers).
+   - Add a new item to the end of the list and another at the second position.
+   - Remove the third item from the list.
+   - Print the list after each operation.
+"""
+Items = ["Milk","Tea Powder","Sugar","Vegetables"," Fruits"]
+Items.append("Curd")
+print(Items)
+Items.pop(2)
+print(Items)
