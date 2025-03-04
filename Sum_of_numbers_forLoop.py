e1f636384db17dc1ab3c5316d0c8542c0e7a5638
Nithin K P <nithinkp0212@gmail.com>
2025-03-04 23:04:12 +0530

    Added while Loop exercises for practice

---
+++ Sum_of_numbers_forLoop.py
@@ -0,0 +1,9 @@
+"""
+**Sum of First 10 Numbers**:
+   - Write a program using a `for` loop that calculates the sum of numbers from 1 to 10.
+"""
+sum = 0
+for i in range(1,11):
+    sum +=i
+    print(sum)
+   
