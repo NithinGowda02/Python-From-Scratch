baee254b8aefd155d0ad84510814357457acb688
Nithin K P <nithinkp0212@gmail.com>
2025-03-04 22:28:02 +0530

    Added while Loop exercises for practice

---
+++ Odd_number.py
@@ -0,0 +1,9 @@
+"""
+**Odd Numbers Printer**:
+   - Create a program that prints all odd numbers between 1 and 20 using a `while` loop.
+"""
+number = 0
+while number <= 20:
+    if number % 2 != 0:
+        print(number)
+    number +=1    
