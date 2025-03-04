49fd615a496b1ef75a62114632a1d8f0f79e74c1
Nithin K P <nithinkp0212@gmail.com>
2025-03-04 23:07:54 +0530

    Added For Loop exercises for practice

---
+++ Tables_ForLoop.py
@@ -0,0 +1,9 @@
+"""
+ **Tables Writting**:
+   - Write a program that prints the Tables from 1 to 20.
+
+"""
+for i in range(1,21):
+    for j in range(1,11):
+        print(f"{i}x{j}={i*j}")
+    print("")    
