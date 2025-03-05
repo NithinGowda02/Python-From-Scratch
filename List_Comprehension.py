f2f8039457f7faad1b40ec5a9c306a283ab67651
Nithin K P <nithinkp0212@gmail.com>
2025-03-05 22:55:57 +0530

    Added List Comprehension exercises for practice

---
+++ List_Comprehension.py
@@ -0,0 +1,9 @@
+"""
+**List Manipulation**:
+   - Create a list of Kannada foods. Use list comprehension to create a new list where each food name is in uppercase.
+
+"""
+Kannada_foods = ["Dosa","Idli","Akki Rotti","Palav","Chapathi"]
+
+Updated_Kannada_foods = [food.upper() for food in Kannada_foods]
+print(Updated_Kannada_foods)
