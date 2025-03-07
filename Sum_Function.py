ec68cca2ad267e65fd574f58b26c62b3431a4a65
Nithin K P <nithinkp0212@gmail.com>
2025-03-07 22:12:21 +0530

    updated sum function for practice

---
+++ Sum_Function.py
@@ -0,0 +1,6 @@
+"""
+**Sum Function**: Write a function `add_numbers(a, b)` that returns the sum of two numbers. Call this function with different values.
+"""
+def add_numbers(a, b):
+    return a + b
+print(f"Sum of two number is >> {add_numbers(23,45)}")
