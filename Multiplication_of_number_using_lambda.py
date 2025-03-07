51d8514101e71ca015aa912eb4b5f85c5d47b712
Nithin K P <nithinkp0212@gmail.com>
2025-03-07 23:10:01 +0530

    Updated Multiplication of two number using Lambda Function

---
+++ Multiplication_of_number_using_lambda.py
@@ -0,0 +1,5 @@
+"""
+**Lambda Function**: Write a lambda function that multiplies two numbers.
+"""
+Multiply = lambda a, b : a * b
+print(f"Multiplication of two number using lambsa Function >> {Multiply(3,6)}")
