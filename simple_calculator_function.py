ff507d719534ffeebb541b875a1ac4ca20e75f96
Nithin K P <nithinkp0212@gmail.com>
2025-03-07 22:11:25 +0530

    Added Simple Calculator using Functions in Python

---
+++ simple_calculator_function.py
@@ -0,0 +1,20 @@
+"""
+**Simple Calculator**: Write a function for each Arithmetic Operation. Call this function with different values.
+"""
+
+def Add(num1, num2):
+    return num1 + num2
+
+def Sub(num1, num2):
+    return num1 - num2
+
+def Multiply(num1, num2):
+    return num1 * num2
+
+def Divide(num1, num2):
+    return num1 / num2
+
+print(f"Sum of two Numbers >> {Add(17,2)}")
+print(f"Substraction of two Numbers >> {Sub(17,2)}")
+print(f"Multiplication of two Numbers >> {Multiply(17,2)}")
+print(f"Division of two Numbers >> {Divide(17,2)}")
