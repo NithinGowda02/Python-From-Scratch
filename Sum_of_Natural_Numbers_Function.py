e596ccb85ab03f4e0ddf2cb2673990414890ed3d
Nithin K P <nithinkp0212@gmail.com>
2025-03-07 22:04:26 +0530

    Program to print sum of n natural numbers

---
+++ Sum_of_Natural_Numbers_Function.py
@@ -0,0 +1,7 @@
+"""
+**Sum of Natural Numbers**: Write a function that prints sum of all n natural numbers.
+"""
+
+def Sum_Of_Natural_Number(n):
+    return sum(range(1,n+1))
+print(f"Sum of Natural Number is >> {Sum_Of_Natural_Number(7)}")            
