31f0b84a2c957418ce4eb441b0901c544aa849c8
Nithin K P <nithinkp0212@gmail.com>
2025-03-07 21:58:23 +0530

    Added Parameterized Greet function

---
+++ Greet_user_function.py
@@ -0,0 +1,10 @@
+"""
+ **Parameterized Greet**: Write a function `greet_user()` that takes a name as input and prints a custom greeting.
+ """
+
+def greet_User(name, age):
+    print(f"Hi {name} you are {age} years old..Welcome to Our Python Channel...!")
+greet_User("Nithin", 22)   
+greet_User("Madan Gowda G R", 20) 
+
+    
