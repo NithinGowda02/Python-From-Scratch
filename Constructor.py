be13658180d909d78938c816c933bedc7d8342f1
Nithin K P <nithinkp0212@gmail.com>
2025-03-08 17:37:46 +0530

    Updated constructor program for  practise

---
+++ Constructor.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# Creating objects
+person1 = Person("Arjun", 30)
+person2 = Person("Megha", 25)
+
+person1.greet()
+person2.greet()
